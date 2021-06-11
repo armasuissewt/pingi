7a0cb0a55a5f7c23894b2063957ee4bbf722fbdf
Adrian Schneider <adrian.schneider.pr@gmail.com>
2021-06-11 15:01:52 +0200

    write ping times into file

--- pingi.py
+++ pingi.py
@@ -3,27 +3,34 @@ from datetime import datetime
 from time import sleep
 import sys
 import argparse
-from typing import Tuple, List
+from typing import Tuple
 
 
-def start_ping_test(addr: str, interval: float):
+def start_ping_test(addr: str, interval: float, file_name: str):
     """
     This function keeps sending pings to a defined
     server address in a specified interval. A simple
     statistics is done.
     :param addr: Server address or ip
     :param interval: Pinging interval in seconds
+    :param file_name: Result file name 
     """
 
-    print("pingi:", addr, ", interval [s]:", interval)
+    print("pingi:", addr, ", interval [s]:", interval, ", result file: ", file_name)
 
     ok = 0
     fail = 0
     ping_time_accum = 0.0
 
+    f = open(file_name, "w")
+    f.close()
+
     while True:
 
         ping_res = ping(addr)
+
+        now = datetime.now()
+        dt_string = now.strftime("%H:%M:%S")
 
         if not ping_res:
             fail += 1
@@ -33,32 +40,37 @@ def start_ping_test(addr: str, interval: float):
             ping_time_accum += ping_res
             sys.stdout.write(".")
 
+            # inefficient but safe
+            f = open(file_name, "a")
+            f.write(dt_string + "," + str(ping_res) + "\n")
+            f.close()
+
         # print out intermediate results every 10th ping
         if (ok + fail) % 10 == 0:
             success_rate = ok / (ok + fail) * 100.0
             avg_ping_reply_time = ping_time_accum / max(1.0, ok)
-            now = datetime.now()
-            dt_string = now.strftime("%H:%M:%S")
             print("  ", dt_string, "-> Success rate =", success_rate, " , Avg. ping reply [s] =", avg_ping_reply_time)
 
         sleep(interval)
 
 
-def parse_pingi_args(argv) -> Tuple[str, float]:
+def parse_pingi_args(argv) -> Tuple[str, float, str]:
     """
     This function parses the pingi command line input.
     :param argv: list of strings
-    :return: ping address, ping interval
+    :return: ping address, ping interval, result file name
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--destination", dest="addr", required=False,
                         help="Ping destination address or IP", default="8.8.8.8")
-    parser.add_argument("-i", "--interval", dest="interval", required=False, help="Ping interval in seconds",
-                        type=float, default=5.0)
+    parser.add_argument("-i", "--interval", dest="interval", required=False,
+                        help="Ping interval in seconds", type=float, default=5.0)
+    parser.add_argument("-o", "--output", dest="file_name", required=False,
+                        help="Result file name (csv)", default="ping.csv")
     args = parser.parse_args(argv)
-    return (args.addr, args.interval)
+    return (args.addr, args.interval, args.file_name)
 
 
 if __name__ == "__main__":
-    ping_addr, ping_interval = parse_pingi_args(sys.argv[1:])
-    start_ping_test(ping_addr, ping_interval)
+    ping_addr, ping_interval, file_name = parse_pingi_args(sys.argv[1:])
+    start_ping_test(ping_addr, ping_interval, file_name)
--- test_pingi.py
+++ test_pingi.py
@@ -4,14 +4,16 @@ import pingi
 class TestPingi(unittest.TestCase):
 
     def test_cmd_parser_default(self):
-        addr, interval = pingi.parse_pingi_args([])
+        addr, interval, file_name = pingi.parse_pingi_args([])
         self.assertEqual(addr, "8.8.8.8")
         self.assertAlmostEqual(interval, 5.0)
+        self.assertEqual(file_name, "ping.csv")
 
     def test_cmd_parser(self):
-        addr, interval = pingi.parse_pingi_args(["-d", "www.blick.ch", "-i", "2.0"])
+        addr, interval, file_name = pingi.parse_pingi_args(["-d", "www.blick.ch", "-i", "2.0", "-o", "abc.csv"])
         self.assertEqual(addr, "www.blick.ch")
         self.assertAlmostEqual(interval, 2.0)
+        self.assertAlmostEqual(file_name, "abc.csv")
 
 if __name__ == '__main__':
     unittest.main()
